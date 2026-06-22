7d091d75c0b240b037024e8a6b8895d44f485d9e
Henrique <henriquesegundo2000@gmail.com>
2026-06-22 20:07:59 -0300

    Exclusão de classes modelo (Pessoa e PessoaDAO)

--- Pessoa.py
+++
@@ -1,9 +0,0 @@
-# Classe que representa a entidade pessoa
-from dataclasses import dataclass
-
-@dataclass
-class Pessoa:
-    codigo: int = None
-    nome: str = ""
-    login: str = ""
-    senha: str = ""
